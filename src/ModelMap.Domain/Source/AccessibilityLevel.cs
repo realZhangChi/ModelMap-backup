d292ce39f6cb310e8eaa34f6322fb8f00bb14688
Zhang Chi <zliming@outlook.com>
2021-05-19 16:25:21 +0800

    :lipstick: linkage get, set and property access level select

---
+++ src/ModelMap.Domain/Source/AccessibilityLevel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelMap.Source
+{
+    public class AccessibilityLevel
+    {
+        // public > protected internal > protected > internal > private protected > private
+        // CS0274: The accessibility modifier of the 'property_accessor' accessor must be more restrictive than the property or indexer 'property'
+        // CS0273: Cannot specify accessibility modifiers for both accessors of the property or indexer 'property/indexer
+        public static IReadOnlyDictionary<int, string> Levels => new Dictionary<int, string>()
+        {
+            {0, string.Empty },
+            {1, "private" },
+            {2, "private protected" },
+            {3, "internal" },
+            {4, "protected" },
+            {5, "protected internal" },
+            {6, "public" },
+        };
+    }
+}
---
+++ src/ModelMap.Domain/Source/EntityPropertyType.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelMap.Source
+{
+    public class EntityPropertyType
+    {
+        public static ICollection<string> Types = new List<string>()
+        {
+            "bool",
+            "byte",
+            "sbyte",
+            "char",
+            "decimal",
+            "double",
+            "float",
+            "int",
+            "uint",
+            "nint",
+            "long",
+            "ulong",
+            "short",
+            "string",
+            "ushort"
+        };
+    }
+}
